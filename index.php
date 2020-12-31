8aa8dc3ad457b6f46e341806f1c2c089e0fe853a
Maria Claudia <mariacladiaperezes@gmail.com>
2020-12-30 21:19:03 -0300

    se modifico el index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<!-- php = hypertext preprocessor -->
-<!-- 1. lenguaje debilmente tipado
-2. facil de instalar y aprender
-3. wordpress esta creado en PHP
-4.popular -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<?php
+//php procesa instrucciones para que html cree las cosas que debe de crear, php nos permite ejecutar codigo html
+//echo funcion para escribir codigo
+  print("<h1>HOLA MUNDO PHP <br></h1>");
+  echo "<h2>TITULO CON PHP PERO CON ECHO</h2>"; 
+?>
+<body>
+
+<!-- se puede agregar distintos bloques de PHP -->
+
+<?php
+  echo "<p>Lorem ipsum dolor sit am</p>"
+?>
+
+  
+</body>
+</html>
