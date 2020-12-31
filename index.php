968e76b7daaf1061786ae5b61434ecfffb861aa7
Maria Claudia <mariacladiaperezes@gmail.com>
2020-12-30 21:30:40 -0300

    se agrego comentarios

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 </head>
 <?php
 //php procesa instrucciones para que html cree las cosas que debe de crear, php nos permite ejecutar codigo html
+/*comentario multilinea*/
+#comentario con hash
 //echo funcion para escribir codigo
   print("<h1>HOLA MUNDO PHP <br></h1>");
   echo "<h2>TITULO CON PHP PERO CON ECHO</h2>"; 
