6969c0cc3d6b5188c7e34e37e38fc52a9df2306a
Maria Claudia <mariacladiaperezes@gmail.com>
2020-12-30 21:48:13 -0300

    se agrega el gititnore y el archivo tipos-variables

---
+++ tipos-variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipos de Datos</title>
+</head>
+<body>
+<?php
+  //tipos de datos
+  //php es un lenguaje debilmente tipado, es decir que no es necesario decirle que tipo de dato va almacenar
+  $edad = 27; 
+  $edad = '<h1>yo tengo 27 años</h1>';
+  $edad = 27.4;
+  $edad = true; // sale 1 
+  $edad = false; // no sale nada
+  echo($edad);
+
+
+?>
+  
+</body>
+</html>
